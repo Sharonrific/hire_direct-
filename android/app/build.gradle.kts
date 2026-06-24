8b08af9929bb3defa5bbd2694223085521a02cc2
Styce <styceplug@gmail.com>
2026-06-24 15:12:57 +0100

    Refactor Android configuration and migrate to Kotlin DSL
    - Migrate `android/app/build.gradle` to `android/app/build.gradle.kts`.
    - Rename package and applicationId from `com.hiredirect.hire_direct` to `com.hiredirect.app`.
    - Update `MainActivity` to extend `FlutterFragmentActivity`.
    - Upgrade Android Gradle Plugin to 8.11.1.
    - Update Java and Kotlin toolchain to version 17.
    - Enable core library desugaring and add `desugar_jdk_libs` dependency.
    - Update Kotlin plugin version in `settings.gradle.kts`.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,25 +1,26 @@
 plugins {
     id("com.android.application")
-    id("com.google.gms.google-services")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("kotlin-android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.hiredirect.hire_direct"
+    namespace = "com.hiredirect.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.hiredirect.hire_direct"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.hiredirect.app"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -28,19 +29,15 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
 }
 
-kotlin {
-    compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
-    }
-}
-
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
 }
